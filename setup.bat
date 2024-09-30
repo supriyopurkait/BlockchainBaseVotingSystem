a5ef85546d8c61d7a03c5674d8eb3207de86a9b4
subhasishmondalwork <subhasishmondalwork+github@gmail.com>
2024-09-30 12:39:29 +0530

    fix: start.bat -> setup.bat + start.bat

--- setup.bat
+++ setup.bat
@@ -8,7 +8,7 @@ start /MIN cmd /k "echo Setting Up Backend... && python -m pip install -r backen
 
 :: Frontend Part
 echo Starting frontend...
-cd frontend
+
 start /MIN cmd /k "echo Setting Up Frontend... && npm i"
 
 echo Backend and frontend are being Setted Up in separate windows.

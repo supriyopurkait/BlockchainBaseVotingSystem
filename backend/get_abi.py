1c7f5b01cbfd4c0f3a09f340a572fd926befb49f
lobstr <djspaul99@gmail.com>
2024-09-30 20:24:20 +0530

    fix: now get abi from artifacts

--- backend/get_abi.py
+++ backend/get_abi.py
@@ -2,14 +2,14 @@ import json
 import os
 import sys
 
-path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'smart_contracts'); sys.path.append(path)
+path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'smart_contracts', 'artifacts'); sys.path.append(path)
 
 # Get the ABI, Bytecode, and Contract Address of the VoterID contract
 def get_abi_voterID():
     try:
-        abi = json.load(open(path + "/scripts/output/voterID/abi.json", "r"))
-        bytecode = open(path + "/scripts/output/voterID/byte_code.txt", "r").read()
-        ca = open(path + "/scripts/output/voterID/CA.txt", "r").read()
+        abi = json.load(open(path + "/voterID/abi.json", "r"))
+        bytecode = open(path + "/voterID/byte_code.txt", "r").read()
+        ca = open(path + "/voterID/CA.txt", "r").read()
     except FileNotFoundError:
         return {"Error: Contract is not deployed yet."}
     return {"contract": "VoterID", "abi": abi, "bytecode": bytecode, "ca": ca}
@@ -17,9 +17,9 @@ def get_abi_voterID():
 # Get the ABI, Bytecode, and Contract Address of the VotingSystem contract
 def get_abi_votingSystem():
     try:
-        abi = json.load(open(path + "/scripts/output/VotingSystem/abi.json", "r"))
-        bytecode = open(path + "/scripts/output/VotingSystem/byte_code.txt", "r").read()
-        ca = open(path + "/scripts/output/VotingSystem/CA.txt", "r").read()
+        abi = json.load(open(path + "/VotingSystem/abi.json", "r"))
+        bytecode = open(path + "/VotingSystem/byte_code.txt", "r").read()
+        ca = open(path + "/VotingSystem/CA.txt", "r").read()
     except FileNotFoundError:
         return {"Error: Contract is not deployed yet."}
     return {"contract": "VotingSystem", "abi": abi, "bytecode": bytecode, "ca": ca}

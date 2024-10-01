39a9e369aa7469d07f92385600814631f8e5a186
subhasishmondalwork <subhasishmondalwork+github@gmail.com>
2024-10-01 18:10:06 +0530

    chore: kills Unnecessary Terminal Windows

--- setup.bat
+++ setup.bat
@@ -4,11 +4,11 @@ echo.
 
 :: Backend Part
 echo Starting backend...
-start /MIN cmd /k "echo Setting Up Backend... && python -m pip install -r backend\requirment.txt && py backend\database.py"
+start /MIN cmd /c "echo Setting Up Backend... && python -m pip install -r backend\requirment.txt && py backend\database.py"
 
 :: Frontend Part
 echo Starting frontend...
 
-start /MIN cmd /k "cd frontend && echo Setting Up Frontend... && npm i"
+start /MIN cmd /c "cd frontend && echo Setting Up Frontend... && npm i"
 
 echo Backend and frontend are being Setted Up in separate windows.

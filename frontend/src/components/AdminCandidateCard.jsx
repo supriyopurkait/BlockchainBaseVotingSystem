f38f8d3dc9d73e981511394e9b517fb7a7e415bf
subhasishmondalwork <subhasishmondalwork+github@gmail.com>
2024-10-02 13:28:53 +0530

    Fix: Admin Candidate Controls UI

--- frontend/src/components/AdminCandidateCard.jsx
+++ frontend/src/components/AdminCandidateCard.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { User } from 'lucide-react';
+import { UserRoundX } from 'lucide-react';
 
-const AdminCandidateCard = ({ user, onVote }) => (
+const AdminCandidateCard = ({ candidate, onRemove }) => (
   <div className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center">
-        <img src={user.photo} alt={user.name} className="w-24 h-24 rounded-full mb-4" />
-    <h3 className="text-xl font-semibold mb-2">{user.name}</h3>
+        <img src={candidate.photo} alt={candidate.name} className="w-24 h-24 rounded-full mb-4" />
+    <h3 onClick={() => alert("hi")} className="text-xl font-semibold mb-2">{candidate.name}</h3>
     <button
-      onClick={() => onVote(user.candidate_id)}
-      className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-full flex items-center"
+      onClick={() => onRemove(candidate.candidate_id)}
+      className="bg-red-500 hover:bg-red-600 text-white font-bold my-2 py-2 px-4 rounded-full flex items-center"
     >
-      Vote
-      <User className="ml-2" size={16} />
+      Remove
+      <UserRoundX className="ml-2" size={16} />
     </button>
   </div>
 );
--- frontend/src/components/AdminCandidateControlsPage.jsx
+++ frontend/src/components/AdminCandidateControlsPage.jsx
@@ -1,79 +1,208 @@
-import React, { useState } from 'react';
-import { X } from 'lucide-react';
+import React, { useState, useEffect } from 'react';
+import CandidateCard from '@/components/AdminCandidateCard';
+import { fetchCandidate } from '@/utils/getDetails';
+import { ethers } from 'ethers';
+import Message from '@/components/AfterVoteMessage';
+import LoadingModal from '@/components/LoadingModal';
+import Face from 'pub/picture/face_img.png';
+import AdminCandidateCard from '@/components/AdminCandidateCard';
+import AdminAddCandidateCard from '@/components/AdminAddCandidateCard';
 
-const AdminCandidateControlsPage = ({ onClose }) => {
-  const [candidates, setCandidates] = useState([]);
-  const [totalVotes, setTotalVotes] = useState(0);
-  const [votesPerCandidate, setVotesPerCandidate] = useState(0);
+const AdminCandidateControlsPage = ({ wallet, VotingSystemContractAddress, VotingSystemABI }) => {
+  const [Candidates, setCandidates] = useState([]);
+  const [dummyCandidates, setDummyCandidates] = useState([
+    {
+      id: 1,
+      photo: Face,
+      name: 'Alice Johnson',
+      candidate_id: '1',
+      area: 'area1',
+      party: 'Progressive Party'
+    },
+    {
+      id: 2,
+      photo: Face,
+      name: 'Bob Smith',
+      candidate_id: '2',
+      area: 'area2',
+      party: 'Liberal Party'
+    },
+    {
+      id: 3,
+      photo: Face,
+      name: 'Charlie Brown',
+      candidate_id: '3',
+      area: 'area3',
+      party: 'Conservative Party'
+    },
+    {
+      id: 4,
+      photo: Face,
+      name: 'Diana Prince',
+      candidate_id: '4',
+      area: 'area4',
+      party: 'Libertarian Party'
+    },
+    {
+      id: 5,
+      photo: Face,
+      name: 'Ethan Hunt',
+      candidate_id: '5',
+      area: 'area5',
+      party: 'Independent'
+    }
+  ]);
+  // const [newCandidate, setNewCandidate] = useState([{
+  //     id: 9999,
+  //     photo: Add,
+  //     name: 'Add Candidate',
+  //     candidate_id: '',
+  //     area: '',
+  //     party: ''
+  // }]);
+  const [loading, setLoading] = useState(true);
+  const [votingLoading, setVotingLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [showAftervoteMessage, setShowAftervoteMessage] = useState(false);
+  const [messageData, setMessageData] = useState('');
+  const [txHash, setTxHash] = useState('');
 
-  const addCandidate = () => {
-    setCandidates([...candidates, `Candidate ${candidates.length + 1}`]); // pass additional data for candidates
-    // write additional web3 mint actions here
+  useEffect(() => {
+    const loadCandidates = async () => {
+      setLoading(true);
+      try {
+        // const fetchedCandidates = await fetchCandidate(wallet);
+        // setUsers(fetchedCandidates);
+        setCandidates(dummyCandidates); // Using Dummy data for testing
+      } catch (err) {
+        setError('Failed to load Candidates. Please try again later.');
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadCandidates();
+  }, [wallet]);
+
+  const handleRemoveCandidate = async (candidateId) => {
+    setVotingLoading(true);
+    try {
+      const RELAYER_URL = 'http://127.0.0.1:5000/api';
+      console.log('Voting for user:', candidateId);
+      const { provider, signer } = wallet;
+      const address = await signer.getAddress();
+
+      if (!VotingSystemContractAddress || !VotingSystemABI || !provider || !signer) {
+        console.error('Invalid contract address, ABI, or wallet provider/signers.');
+        return;
+      }
+
+      const contract = new ethers.Contract(VotingSystemContractAddress, VotingSystemABI, signer);
+      const nonce = await contract.nonces(address);
+      const functionSignature = contract.interface.encodeFunctionData('vote', [candidateId]);
+
+      const messageHash = ethers.solidityPackedKeccak256(
+        ['address', 'uint256', 'bytes'],
+        [address, nonce, functionSignature]
+      );
+      const messageHashBinary = ethers.getBytes(messageHash);
+      const signature = await signer.signMessage(messageHashBinary);
+      const { r, s, v } = ethers.Signature.from(signature);
+
+      const response = await fetch( `${RELAYER_URL}/execute-meta-tx`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ userAddress: address, functionSignature, r, s, v })
+      });
+
+      const data = await response.json();
+      if (data.status === 'success') {
+        console.log('Voting successful:', data.txHash);
+        setMessageData(`Congratulations! You successfully voted.`);
+        setTxHash(data.txHash);
+      } else {
+        setMessageData(data.message || 'An error occurred while voting. Please try again.');
+      }
+      setShowAftervoteMessage(true);
+    } catch (error) {
+      setMessageData('An unexpected error occurred. Please try again later.');
+      console.error(error);
+      setShowAftervoteMessage(true);
+    } finally {
+      setVotingLoading(false);
+    }
+  };
+  const handleAddCandidate = async (candidateId) => {
+    setVotingLoading(true);
+    try {
+      const RELAYER_URL = 'http://127.0.0.1:5000/api';
+      console.log('Voting for user:', candidateId);
+      const { provider, signer } = wallet;
+      const address = await signer.getAddress();
+
+      if (!VotingSystemContractAddress || !VotingSystemABI || !provider || !signer) {
+        console.error('Invalid contract address, ABI, or wallet provider/signers.');
+        return;
+      }
+
+      const contract = new ethers.Contract(VotingSystemContractAddress, VotingSystemABI, signer);
+      const nonce = await contract.nonces(address);
+      const functionSignature = contract.interface.encodeFunctionData('vote', [candidateId]);
+
+      const messageHash = ethers.solidityPackedKeccak256(
+        ['address', 'uint256', 'bytes'],
+        [address, nonce, functionSignature]
+      );
+      const messageHashBinary = ethers.getBytes(messageHash);
+      const signature = await signer.signMessage(messageHashBinary);
+      const { r, s, v } = ethers.Signature.from(signature);
+
+      const response = await fetch( `${RELAYER_URL}/execute-meta-tx`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ userAddress: address, functionSignature, r, s, v })
+      });
+
+      const data = await response.json();
+      if (data.status === 'success') {
+        console.log('Voting successful:', data.txHash);
+        setMessageData(`Congratulations! You successfully voted.`);
+        setTxHash(data.txHash);
+      } else {
+        setMessageData(data.message || 'An error occurred while voting. Please try again.');
+      }
+      setShowAftervoteMessage(true);
+    } catch (error) {
+      setMessageData('An unexpected error occurred. Please try again later.');
+      console.error(error);
+      setShowAftervoteMessage(true);
+    } finally {
+      setVotingLoading(false);
+    }
   };
 
-  const removeCandidate = (index) => {
-    setCandidates(candidates.filter((_, i) => i !== index));
-    // write additional web3 burn actions here
-  };
-
-  const declareVotesPerCandidate = () => {
-    setVotesPerCandidate(votesPerCandidate + 1);
-    // write additional web3 transactions count per candidate actions here
-  };
-
-  const declareTotalVotes = () => {
-    setTotalVotes(totalVotes + 1);
-    // write additional web3 transactions count actions here
-  };
+  if (loading) return <div>Loading users...</div>;
+  if (error) return <div>{error}</div>;
 
   return (
-    <div className="flex flex-col items-center p-4">
-      <h1 className="text-4xl font-bold mb-8">Admin Candidate Controls</h1>
-      <button onClick={onClose} className="relative top-[-3.7rem] right-[-16rem]">
-          <X className="bg-red-500 hover:bg-red-600 text-white hover:text-gray-700 rounded-full" size={24} />
-      </button>
-      <div className="flex flex-col items-center mb-4">
-        <h2 className="text-lg font-semibold mb-2">Candidates:</h2>
-        <ul className="list-none">
-          {candidates.map((candidate, index) => (
-            <li key={index} className="flex items-center mb-2 px-2">
-              {candidate}
-              <button
-                className="w-fit bg-red-500 hover:bg-red-600 text-white font-bold my-2 mx-4 py-2 px-4 rounded"
-                onClick={() => removeCandidate(index)}
-              >
-                Remove
-              </button>
-            </li>
-          ))}
-        </ul>
-        <button
-          className="w-fit bg-green-500 hover:bg-green-600 text-white font-bold my-2 py-2 px-4 rounded"
-          onClick={addCandidate}
-        >
-          Add Candidate
-        </button>
+    <div className="container mx-auto px-4 py-8">
+      <h2 className="text-3xl font-bold mb-8 text-center">Candidates:</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {Candidates.map(candidate => (
+          <AdminCandidateCard key={candidate.candidate_id} candidate={candidate} onRemove={handleRemoveCandidate} />
+        ))}
+        {<AdminAddCandidateCard onAdd={handleAddCandidate} />}
       </div>
-      <div className="flex flex-col items-center mb-4">
-        <h2 className="text-lg font-semibold mb-2">Votes Per Candidate:</h2>
-        <p>{votesPerCandidate}</p>
-        <button
-          className="w-fit bg-blue-500 hover:bg-blue-600 text-white font-bold my-2 py-2 px-4 rounded"
-          onClick={declareVotesPerCandidate}
-        >
-          Declare Votes Per Candidate
-        </button>
-      </div>
-      <div className="flex flex-col items-center">
-        <h2 className="text-lg font-semibold mb-2">Total Votes:</h2>
-        <p>{totalVotes}</p>
-        <button
-          className="w-fit bg-blue-700 hover:bg-blue-800 text-white font-bold my-2 py-2 px-4 rounded"
-          onClick={declareTotalVotes}
-        >
-          Declare Total Votes
-        </button>
-      </div>
+      {votingLoading && <LoadingModal modalVisible={votingLoading} task="Submitting your vote..." />}
+      {showAftervoteMessage && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <Message
+            data={messageData}
+            txhash={txHash ? `${txHash}` : null}
+            onClose={() => setShowAftervoteMessage(false)}
+          />
+        </div>
+      )}
     </div>
   );
 };

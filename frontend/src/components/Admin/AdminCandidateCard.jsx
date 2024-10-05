27117f85313e113a7517269a0812b6af8913316e
lobstr <djspaul99@gmail.com>
2024-10-05 20:49:59 +0530

    feat: candidate details

--- frontend/src/components/Admin/AdminCandidateCard.jsx
+++ frontend/src/components/Admin/AdminCandidateCard.jsx
@@ -4,7 +4,10 @@ import { UserRoundX } from 'lucide-react';
 const AdminCandidateCard = ({ candidate, onRemove }) => (
   <div className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center">
         <img src={candidate.photo} alt={candidate.name} className="w-24 h-24 rounded-full mb-4" />
-    <h3 onClick={() => alert("hi")} className="text-xl font-semibold mb-2">{candidate.name}</h3>
+    <h3 onClick={() => alert("hi")} className="text-xl font-semibold mb-2"><b>Name:</b> {candidate.name}</h3>
+    <h4 className="text-lg font-semibold mb-2"><b>Party:</b> {candidate.party}</h4>
+    <h4 className="text-lg font-semibold mb-2"><b>Area:</b> {candidate.area}</h4>
+
     <button
       onClick={() => onRemove(candidate.candidate_id)}
       className="bg-red-500 hover:bg-red-600 text-white font-bold my-2 py-2 px-4 rounded-full flex items-center"

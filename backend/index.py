fd8578487ed4447cd911f24c9242f798dd16a699
subhasishmondalwork <subhasishmondalwork+github@gmail.com>
2024-11-01 18:41:57 +0530

    fix: added proper import instruction for kyc and fixed dependency

--- backend/index.py
+++ backend/index.py
@@ -8,3 +8,9 @@ import sqlite3
 from execute_meta_tx import execute_meta_tx
 from ipfs_ops import upload_to_ipfs, unpin_from_ipfs
 from vote_calculations import get_candidates_by_area, determine_winners, process_results, get_vote_state
+import numpy as np
+import cv2
+import easyocr
+from sklearn.metrics.pairwise import cosine_similarity
+from mtcnn import MTCNN
+from keras_facenet import FaceNet

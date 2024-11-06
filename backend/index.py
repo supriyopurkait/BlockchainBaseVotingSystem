a14aae4f56564f72a5f94011674a0170693c3d57
lobstr <djspaul99@gmail.com>
2024-11-06 23:42:51 +0530

    import faceapp

--- backend/index.py
+++ backend/index.py
@@ -7,7 +7,7 @@ from get_user_details import get_details
 from execute_meta_tx import execute_meta_tx
 from ipfs_ops import upload_to_ipfs, unpin_from_ipfs
 from vote_calculations import get_candidates_by_area, determine_winners, process_results, get_vote_state
-from kyc_processor import initialize_face_analyzer, detect_and_embed_face, are_same_person, extract_text_from_image, check_substrings_in_text, searchname_in_list
+from kyc_processor import initialize_face_analyzer, detect_and_embed_face, are_same_person, extract_text_from_image, check_substrings_in_text, searchname_in_list, faceapp
 
 ## Modules From PIP
 import os
